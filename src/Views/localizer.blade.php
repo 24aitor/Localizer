b1047ed648d68d3b989fccf24de08bfd96e94650
24aitor <1aitorribaarchilla@gmail.com>
2016-11-28 16:44:32 +0100

    Added Config & Views

---
+++ src/Views/localizer.blade.php
@@ -0,0 +1,48 @@
+@extends('laralang::template')
+@section('title', 'Translations - Laralang')
+@section('content')
+<center>
+<br><br>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6 offset-lg-3">
+			<h1>Localizer</h1>
+		</div>
+		<br><br>
+		<br><br>
+		<div class="container">
+
+			<div class="row">
+			@foreach(Laralang::allLanguages() as $key => $value)
+				<?php
+					$flag = $value;
+					$array = ['en' => 'gb','zh' => 'cn', 'ja' => 'jp', 'ca' => 'img'];
+					if (array_key_exists($value, $array)) {
+						$flag = $array[$value];
+					}
+				 ?>
+				 <div class="col-xs-12 col-sm-6 col-md-3 col-lg-2 col-xl" style="padding-bottom:10px">
+				 <button type="button" class="btn btn-secondary">
+				 @if($flag == 'img')
+				 <img src="{{asset('vendor/aitor24/Localizer/Flags/'.$value.'.jpg')}}" style="height:16px;">
+				 @else
+				 <span class="{{'flag-icon flag-icon-'.$flag}}" ></span>
+				 @endif
+				 &nbsp;
+				{{$key}}
+				</button>
+			</div>
+			@endforeach
+			</div>
+
+		</div>
+	</div>
+</div>
+<br><br>
+<a href="https://github.com/24aitor/Localizer">Localizer on github</a>
+<br><br>
+</center>
+
+
+
+@endsection
---
+++ src/Views/template.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+
+        <!-- CDN of icons -->
+        <link rel="stylesheet" href="http://cdn.materialdesignicons.com/1.7.22/css/materialdesignicons.min.css">
+
+        <!-- CDN of flags -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.7.0/css/flag-icon.css" />
+
+        <title>@yield('title')</title>
+
+    </head>
+    <body>
+
+        @yield('content')
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+
+
+        @yield('js')
+    </body>
+</html>
