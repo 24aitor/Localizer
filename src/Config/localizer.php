caf1d169c59941620b3ad409e537495b3e3622c1
Aitor Riba Archilla <info@aitorriba.com>
2016-12-07 20:58:42 +0000

    Apply fixes from StyleCI

--- src/Config/localizer.php
+++ src/Config/localizer.php
@@ -7,11 +7,11 @@ return [
     |--------------------------------------------------------------------------
     */
     'default'   => [
-        'routes'         => true,
+        'routes'             => true,
         'select_lang_routes' => true, // If routes is false has no effect
-        'set_auto_lang' => true,
-        'default_lang' => 'en', // If set_auto_lang is true has no effect
-        'prefix'         => 'localizer',
-        'middleware'     => Aitor24\Localizer\Middlewares\LocalizerMiddleware::class,
+        'set_auto_lang'      => true,
+        'default_lang'       => 'en', // If set_auto_lang is true has no effect
+        'prefix'             => 'localizer',
+        'middleware'         => Aitor24\Localizer\Middlewares\LocalizerMiddleware::class,
     ],
 ];
