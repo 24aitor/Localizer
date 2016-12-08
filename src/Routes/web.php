a6005841a4a4a66af2ea7c89e5c52523e77d3800
24aitor <1aitorribaarchilla@gmail.com>
2016-12-08 14:16:24 +0100

    Passed routes by localizer middleware

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 if (config('localizer.default.routes')) {
-    Route::group(['middleware' => 'web', 'as' => 'localizer::', 'prefix' => config('localizer.default.prefix'), 'namespace' => 'Aitor24\Localizer\Controllers'], function () {
+    Route::group(['middleware' => ['web', 'localizer.middleware'], 'as' => 'localizer::', 'prefix' => config('localizer.default.prefix'), 'namespace' => 'Aitor24\Localizer\Controllers'], function () {
         Route::get('/set/{locale}/', 'LocalizerController@set')->name('setLocale');
         Route::get('/set/here/{locale}/', 'LocalizerController@setHere')->name('setLocaleHere');
         if (config('localizer.default.select_lang_routes')) {
