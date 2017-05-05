c6974448724d49ec19b916905895c68fa2918bbb
Aitor Riba Archilla <contact@aitorriba.com>
2017-05-05 21:29:29 +0200

    Update localizer.php

--- src/Config/localizer.php
+++ src/Config/localizer.php
@@ -11,6 +11,6 @@ return [
     'set_auto_lang'      => true,
     'default_lang'       => 'en', // If set_auto_lang is true has no effect
     'prefix'             => 'localizer',
-    'allowed_langs'      => ['en', 'ca', 'es', 'de', 'it'], // If is empty only default_lang will be allowed,
+    'allowed_langs'      => ['en', 'ca', 'es', 'fr', 'de', 'it'], // If is empty only default_lang will be allowed,
     'middleware'         => Aitor24\Localizer\Middlewares\LocalizerMiddleware::class,
 ];
