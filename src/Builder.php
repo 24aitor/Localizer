fb12d86f0ed6bbc84e9bf3437216594e62668e98
24aitor <contact@aitorriba.com>
2017-05-06 17:23:24 +0200

    Tweaks and added function getCode()

--- src/Builder.php
+++ src/Builder.php
@@ -10,7 +10,7 @@ class Builder
      * Get all allowed languages.
      *
      * @return array
-     */
+     **/
     public static function allowedLanguages()
     {
         if (config('localizer.allowed_langs')) {
@@ -21,31 +21,56 @@ class Builder
     }
 
     /**
-     * Returns add names for arrays with only codes an return an array as [$code => $language].
+     * Add names to an array of language codes as [$code => $language].
+     *
+     * @param array $codes
+     *
+     * @return array
+     **/
+    public static function addNames($codes)
+    {
+        // Read and decode JSON
+        $json_data = json_decode(file_get_contents(__DIR__.'/languages.json'), true);
+
+        $array = [];
+
+        // Generate an array with $code as key and $code language as value
+        foreach ($codes as $code) {
+            $lang_name = 'Unknoun';
+            foreach ($json_data as $lang_data) {
+                if ($lang_data['code'] == $code) {
+                    $lang_name = $lang_data['name'];
+                }
+            }
+            $array[$code] = $lang_name;
+        }
+
+        return $array;
+    }
+
+    /**
+     * Add names to an array of language codes as [$language => $code].
      *
      * @param array $langs
      *
      * @return array
-     */
-    public static function addNames($langs)
+     **/
+    public static function addCodes($langs)
     {
-        // Read JSON file
-        $json = file_get_contents(__DIR__.'/languages.json');
-
-        //Decode JSON
-        $json_data = json_decode($json, true);
+        // Read and decode JSON
+        $json_data = json_decode(file_get_contents(__DIR__.'/languages.json'), true);
 
         $array = [];
 
-        //Generate an array with $lang code as key and name as value
+        // Generate an array with $lang as key and $lang code as value
         foreach ($langs as $lang) {
-            $lang_name = 'Unknoun';
+            $code = 'unk';
             foreach ($json_data as $lang_data) {
-                if ($lang_data['code'] == $lang) {
-                    $lang_name = $lang_data['name'];
+                if ($lang_data['name'] == $lang) {
+                    $code = $lang_data['code'];
                 }
             }
-            $array[$lang] = $lang_name;
+            $array[$lang] = $code;
         }
 
         return $array;
@@ -57,7 +82,7 @@ class Builder
      * @param string $code
      *
      * @return string
-     */
+     **/
     public static function setRoute($code)
     {
         return route('localizer::setLocale', ['locale' => $code]);
@@ -69,17 +94,27 @@ class Builder
      * @param string $code
      *
      * @return string
-     */
+     **/
     public static function setRouteHome($code)
     {
         return route('localizer::setLocaleHome', ['locale' => $code]);
     }
 
     /**
-     * Returns  the current language name.
+     * Returns the current language code.
      *
      * @return string
-     */
+     **/
+    public static function getCode($name = self::getLanguage())
+    {
+        return self::addCodes([$name])[$name];
+    }
+
+    /**
+     * Returns the language name.
+     *
+     * @return string
+     **/
     public static function getLanguage($code = App::getLocale())
     {
         return self::addNames([$code])[$code];
