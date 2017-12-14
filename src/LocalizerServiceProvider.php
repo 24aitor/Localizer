2ce3e9b0f11ced53feda59136e9e2b248d653647
Aitor Riba Archilla <contact@aitorriba.com>
2017-12-14 18:07:36 +0100

    Update LocalizerServiceProvider.php

--- src/LocalizerServiceProvider.php
+++ src/LocalizerServiceProvider.php
@@ -29,12 +29,12 @@ class LocalizerServiceProvider extends ServiceProvider
         $router->aliasMiddleware('localizer', config('localizer.middleware'));
         $this->app->register(IdentifyServiceProvider::class);
 
-        if (! class_exists('AddLocaleColumn')) {
-            $this->loadMigrationsFrom(__DIR__.'/Migrations', 'localizer');
-            
-            $this->publishes([
+        $this->publishes([
                 __DIR__.'/Migrations/2016_11_28_115831_add_locale_column.php' => $this->app->databasePath().'/migrations/'.date('Y_m_d_His', time()).'_add_locale_column.php',
-            ], 'localizer_migrations');
+        ], 'localizer_migrations');
+        
+        if (!config('localizer.dynamic_migration_name')) {            
+            $this->loadMigrationsFrom(__DIR__.'/Migrations', 'localizer');
         }
     }
 
